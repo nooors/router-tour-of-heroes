11c8b2104965970aa7b874dfbb0e6e6324f801f8
Jose Nortes <nooors@gmail.com>
2022-06-13 14:13:19 +0200

    the app before the milestone1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
+import { AppComponent } from "./app.component";
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
+import { HeroListComponent } from "./hero-list/hero-list.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+  { path: "crisis-center", component: CrisisListComponent },
+  { path: "heroes", component: HeroListComponent },
+  { path: "", redirectTo: "/heroes", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }, //<-- debugging puposes only
+    ),
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
